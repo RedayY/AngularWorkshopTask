41e359526645e4ac58914be8cf1768164620912a
RedayY <reday_nbg@icloud.com>
2020-09-09 15:53:04 +0200

    Initial Component and Module Structure
    Added Books and About Modules and Component

---
+++ RayBookStore/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BooksModule } from './books/books.module';
+import { AboutModule } from './about/about.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BooksModule,
+    AboutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
