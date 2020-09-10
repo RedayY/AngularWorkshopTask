8579d7cf61f7986fa17d50e0b322d1df98fb63fd
RedayY <reday_nbg@icloud.com>
2020-09-10 11:57:13 +0200

    Output Task

--- RayBookStore/src/app/title-box/title-box.component.ts
+++ RayBookStore/src/app/title-box/title-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-title-box',
@@ -7,6 +7,11 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TitleBoxComponent implements OnInit {
   @Input() title: string;
+  @Output() titleClick = new EventEmitter<string>();
+
+  sendBookEvent() {
+    this.titleClick.emit('bookEvent');
+  }
 
   constructor() {}
 
