43fbf700ee50ed7f3703f292c6ffb17269e54374
RedayY <reday_nbg@icloud.com>
2020-09-10 09:08:20 +0200

    Added Angular Material

--- RayBookStore/src/app/app.module.ts
+++ RayBookStore/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
 import { NavigationComponent } from './navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NavigationComponent } from './navigation/navigation.component';
     BrowserModule,
     AppRoutingModule,
     BooksModule,
-    AboutModule
+    AboutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
