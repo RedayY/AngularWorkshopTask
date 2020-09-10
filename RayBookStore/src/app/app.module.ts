79e678005670a7a2fb31801e3418a9268fc06fc4
RedayY <reday_nbg@icloud.com>
2020-09-10 08:32:19 +0200

    Added Nav Module
    Top Navigation for Site

--- RayBookStore/src/app/app.module.ts
+++ RayBookStore/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +19,7 @@ import { AboutModule } from './about/about.module';
     AboutModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [NavigationComponent]
 })
 export class AppModule { }
