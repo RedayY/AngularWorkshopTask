05d052dda713dd455c7f9d33091eaf2aa1d08f80
RedayY <reday_nbg@icloud.com>
2020-09-11 11:13:35 +0200

    Added Routing

--- RayBookStore/src/app/books/books-routing.module.ts
+++ RayBookStore/src/app/books/books-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooklistComponent } from './booklist/booklist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BooklistComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BooksRoutingModule { }
+export class BooksRoutingModule {}
